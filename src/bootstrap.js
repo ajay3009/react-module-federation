53f9bc10a228fb6643b527a0eeb4c69a2a1fde8b
Ajay Pandit <ajaysandy@gmail.com>
2024-01-28 20:47:21 +0530

    Initial Commit

---
+++ src/bootstrap.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MicroApp from "microApp/MicroApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    ddddd
+    <MicroApp />
+  </React.StrictMode>,
+  document.getElementById("app")
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    publicPath: "auto",
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "mainApp",
+      filename: "remoteEntry.js",
+      remotes: {
+        microApp: "microApp@http://localhost:3001/remoteEntry.js",
+      },
+    }),
+  ],
+};
